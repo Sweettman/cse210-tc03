db254964ece88c27d4745f2b46d4d6d152b7f80b
Sweettman <91554870+Sweettman@users.noreply.github.com>
2021-10-01 19:44:39 -0600

    changes

--- Python code/Wilde.py
+++ Python code/Wilde.py
@@ -1,2 +1,4 @@
 def main():
   print("Hello from Gerin Wilde")
+
+main()
--- Python code/main.py
+++ Python code/main.py
@@ -1,4 +1,22 @@
-import LeBaron
+import Wilde, LeBaron
+
+def main():
+  l = LeBaron
+  w = Wilde
+  
+  print("Welcome to our collaborative program.")
+  
+  print(w)
+  print(l)
+
+#def name(LeBaron):
+
+  #return(LeBaron)
+
+#def name1(Wilde):
+
+  #return(Wilde)
 
 
-print(LeBaron.welcom)
+
+main()
